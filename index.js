09007d5a39199f876892670dc54a6b9a098e1b52
Max O'Cull <maxattax97@gmail.com>
2018-06-21 13:25:24 -0400

    Mostly implemented

--- index.js
+++ index.js
@@ -1,36 +1,162 @@
-import dotProp from 'dot-prop';
-import Promise from 'bluebird';
+const fs = require('fs');
+const Promise = require('bluebird');
+const dotProp = require('dot-prop');
 
 class Config {
   constructor() {
     this.data = {};
+    this.log = [];
+    this.locked = false;
+    this.environment = 'default';
+    this.configDir = `${process.cwd()}/config`;
+    this.instance = 0;
+    this.readEnvironment();
   }
 
   load(pathOrObj) {
-    return new Promise((resolve) => {
-      this.data = pathOrObj;
-      resolve();
+    return new Promise((resolve, reject) => {
+      if (typeof pathOrObj === 'object') {
+        this.log.push({
+          type: 'object',
+          contents: pathOrObj,
+        });
+        this.data = pathOrObj;
+        resolve();
+      } else if (typeof pathOrObj === 'string') {
+        fs.readFile(pathOrObj, (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            try {
+              const parsed = JSON.parse(data.toString());
+              this.log.push({
+                type: 'file',
+                path: pathOrObj,
+                contents: parsed,
+              });
+              this.overlay(parsed);
+              resolve();
+            } catch (err2) {
+              reject(err2);
+            }
+          }
+        });
+      } else {
+        let instanceStr = '';
+        if (this.instance !== 0 && this.instance !== '0') {
+          instanceStr = `-${instanceStr}`;
+        }
+
+        this.load(`${this.configDir}${this.environment}${instanceStr}.json`)
+          .then(resolve).catch(reject);
+      }
     });
   }
 
   loadSync(pathOrObj) {
-    this.data = pathOrObj;
+    if (typeof pathOrObj === 'object') {
+      this.log.push({
+        type: 'object',
+        contents: pathOrObj,
+      });
+      this.overlay(pathOrObj);
+    } else if (typeof pathOrObj === 'string') {
+      const data = fs.readFileSync(pathOrObj);
+      try {
+        const parsed = JSON.parse(data.toString());
+        this.log.push({
+          type: 'file',
+          path: pathOrObj,
+          contents: parsed,
+        });
+        // this.data = parsed;
+        this.overlay(parsed);
+      } catch (err2) {
+        throw err2;
+      }
+    } else {
+      let instanceStr = '';
+      if (this.instance !== 0 && this.instance !== '0') {
+        instanceStr = `-${instanceStr}`;
+      }
+
+      this.loadSync(`${this.configDir}${this.environment}${instanceStr}.json`);
+    }
+  }
+
+  overlay(obj) {
+    Object.keys(obj).forEach((key) => {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        if (obj[key] !== undefined) {
+          this.data[key] = obj[key];
+        }
+      }
+    });
+  }
+
+  readEnvironment() {
+    if (process.env.NODE_CONFIG_ENV) {
+      this.environment = process.env.NODE_CONFIG_ENV;
+    }
+
+    if (process.env.NODE_CONFIG_DIR) {
+      this.configDir = process.env.NODE_CONFIG_DIR;
+    }
+
+    if (process.env.NODE_APP_INSTANCE) {
+      this.instance = process.env.NODE_APP_INSTANCE;
+    }
+  }
+
+  save(path) {
+    return new Promise((resolve, reject) => {
+      this.lock();
+      fs.writeFile(path, JSON.stringify(this.data, null, 2), (err) => {
+        this.unlock();
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
+  }
+
+  saveSync(path) {
+    this.lock();
+    fs.writeFileSync(path, JSON.stringify(this.data, null, 2));
+    this.unlock();
+  }
+
+  all() {
+    return this.data;
   }
 
   get(property) {
     return dotProp.get(this.data, property);
   }
 
-  set(property) {
-    return dotProp.set(this.data, property);
+  set(property, value) {
+    if ((!this.locked) && (value !== undefined)) {
+      dotProp.set(this.data, property, value);
+    }
   }
 
   delete(property) {
-    return dotProp.delete(this.data, property);
+    if (!this.locked) {
+      dotProp.delete(this.data, property);
+    }
   }
 
   has(property) {
     return dotProp.set(this.data, property);
+  }
+
+  lock() {
+    this.locked = true;
+  }
+
+  unlock() {
+    this.locked = false;
   }
 }
 
--- test/main.js
+++ test/main.js
@@ -1,8 +1,120 @@
-import { expect } from 'chai';
+/* eslint-env mocha */
+const path = require('path');
+const { expect } = require('chai');
+const Config = require('../index.js');
+const fs = require('fs');
 
 describe('Main', () => {
-  it('should succeed on true', () => {
-    expect(true).to.equal(true);
+  describe('Loading', () => {
+    it('should load files asynchronously', (done) => {
+      const conf = new Config();
+      conf.load(path.join(__dirname, 'configs', 'default.json')).then(() => {
+        const name = conf.get('Name');
+        expect(name).to.equal('John');
+        done();
+      });
+    });
+
+    it('should load files synchronously', () => {
+      const conf = new Config();
+      conf.loadSync(path.join(__dirname, 'configs', 'default.json'));
+      const name = conf.get('Name');
+      expect(name).to.equal('John');
+    });
+  });
+
+  describe('Access', () => {
+    it('should access object properties', (done) => {
+      const conf = new Config();
+      conf.load(path.join(__dirname, 'configs', 'default.json')).then(() => {
+        const name = conf.get('Object.Name');
+        expect(name).to.equal('Jake');
+        done();
+      });
+    });
+
+    it('should access arrays', (done) => {
+      const conf = new Config();
+      conf.load(path.join(__dirname, 'configs', 'default.json')).then(() => {
+        const values = conf.get('Values');
+        expect(values).to.deep.equal([1, 2, 3, 9, 7, 8]);
+        done();
+      });
+    });
+
+    it('should set and keep values', (done) => {
+      const conf = new Config();
+      conf.load(path.join(__dirname, 'configs', 'default.json')).then(() => {
+        let float = conf.get('Float');
+        expect(float).to.equal(0.01);
+        conf.set('Float', 23.4);
+        float = conf.get('Float');
+        expect(float).to.equal(23.4);
+        done();
+      });
+    });
+
+    it('should not set undefined', (done) => {
+      const conf = new Config();
+      conf.load(path.join(__dirname, 'configs', 'default.json')).then(() => {
+        let float = conf.get('Float');
+        expect(float).to.equal(0.01);
+        conf.set('Float', undefined);
+        float = conf.get('Float');
+        expect(float).to.equal(0.01);
+        done();
+      });
+    });
+
+    it('should deny setting when locked', (done) => {
+      const conf = new Config();
+      conf.load(path.join(__dirname, 'configs', 'default.json')).then(() => {
+        const float = conf.get('Float');
+        expect(float).to.equal(0.01);
+        conf.lock();
+        conf.set('Float', 23.4);
+        expect(conf.get('Float')).to.equal(0.01);
+        conf.unlock();
+        conf.set('Float', 23.4);
+        expect(conf.get('Float')).to.equal(23.4);
+        done();
+      });
+    });
+  });
+
+  describe('Saving', () => {
+    it('should save files asynchronously', (done) => {
+      const conf = new Config();
+      conf.load(path.join(__dirname, 'configs', 'default.json')).then(() => {
+        conf.set('Name', 'Elizabeth');
+        expect(conf.get('Name')).to.equal('Elizabeth');
+        conf.save(path.join(__dirname, 'configs', 'saveAsync.json')).then(() => {
+          // eslint-disable-next-line
+          fs.access(path.join(__dirname, 'configs', 'saveAsync.json'), fs.F_OK | fs.R_OK, (err) => {
+            if (err) {
+              done(err);
+            } else {
+              conf.load(path.join(__dirname, 'configs', 'saveAsync.json')).then(() => {
+                expect(conf.get('Name')).to.equal('Elizabeth');
+                done();
+              });
+            }
+          });
+        });
+      });
+    });
+
+    it('should save files synchronously', () => {
+      const conf = new Config();
+      conf.loadSync(path.join(__dirname, 'configs', 'default.json'));
+      conf.set('Name', 'Elizabeth');
+      expect(conf.get('Name')).to.equal('Elizabeth');
+      conf.saveSync(path.join(__dirname, 'configs', 'saveSync.json'));
+      // eslint-disable-next-line
+      fs.accessSync(path.join(__dirname, 'configs', 'saveSync.json'), fs.F_OK | fs.R_OK);
+      conf.loadSync(path.join(__dirname, 'configs', 'saveSync.json'));
+      expect(conf.get('Name')).to.equal('Elizabeth');
+    });
   });
 });
 
